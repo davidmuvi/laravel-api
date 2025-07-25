0b103c7b83b45c4d095af4c5fbc98ae98477e868
davidmuvi <davidmunozvillalba@gmail.com>
2025-07-25 22:32:26 +0200

    Closure deleted

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 use App\Models\Restaurant;
 
 Route::get('/debug-restaurants', function() {
